2fdadfdb33bffbfb611701d04331d527d3481190
johant87 <jtillaart@hotmail.com>
2016-03-29 14:04:56 +0200

    calculate time remaning in minutes

--- script.js
+++ script.js
@@ -2,4 +2,20 @@
   var timeLeft = 15;
   var currentTime = Date.parse(new Date());
   var deadline = new Date(currentTime + timeLeft*60*1000);
-  console.log(deadline)
+
+function countDownTime(deadline){
+  var t = Date.parse(deadline) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+console.log(countDownTime(deadline).minutes)
